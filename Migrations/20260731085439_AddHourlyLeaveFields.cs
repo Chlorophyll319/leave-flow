7e5d5e884acdb48559abdddf80f736161033d786
Chlorophyll319 <kcnfoggy54@gmail.com>
2026-07-31 16:55:34 +0800

    feat(leave-requests): 資料模型新增以小時計欄位並產生 Migration

---
+++ Migrations/20260731085439_AddHourlyLeaveFields.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHourlyLeaveFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "EndTime",
+                table: "LeaveRequests",
+                type: "time without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHourly",
+                table: "LeaveRequests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "StartTime",
+                table: "LeaveRequests",
+                type: "time without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "IsHourly",
+                table: "LeaveRequests");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "LeaveRequests");
+        }
+    }
+}
